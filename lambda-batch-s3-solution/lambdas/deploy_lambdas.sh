7b97ec6c31c79ae05e4862a7614fe109fcd72a6f
ahsin-s <ashabbi00@gmail.com>
2025-05-27 12:31:50 -0400

    fix syntax error

--- lambda-batch-s3-solution/lambdas/deploy_lambdas.sh
+++ lambda-batch-s3-solution/lambdas/deploy_lambdas.sh
@@ -4,7 +4,7 @@ deploy_lambdas() {
   lambdas=("pre-signed-url-generator")
   for lambda in "${lambdas[@]}"; do
     rm -f code.zip
-    rm -f lambda_function.py .
+    rm -f lambda_function.py
     cp $lambda/lambda_function.py .
     zip code.zip "lambda_function.py"
     aws lambda update-function-code --function-name $lambda --zip-file fileb://code.zip --profile $AWS_DEPLOYMENT_PROFILE
