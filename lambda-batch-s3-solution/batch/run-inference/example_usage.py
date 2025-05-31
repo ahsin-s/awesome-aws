7c8fcb53c37e9522d3a6ebd99370b2aaa7efdddd
ahsin-s <ashabbi00@gmail.com>
2025-05-30 23:46:12 -0400

    remove unused files

--- lambda-batch-s3-solution/batch/run-inference/example_usage.py
+++
@@ -1,8 +0,0 @@
-import whisper
-
-model = whisper.load_model('whisper-small.pt')
-# Run transcription
-result = model.transcribe("extractedaudio.mp3")
-
-# Print the transcription
-print(result["text"])
