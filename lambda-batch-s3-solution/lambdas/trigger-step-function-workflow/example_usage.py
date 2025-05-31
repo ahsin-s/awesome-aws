f2690118e28cadc9c63c586b0d1bf5c7ff5110a7
ahsin-s <ashabbi00@gmail.com>
2025-05-30 23:14:18 -0400

    minor change in example_usage.py

--- lambda-batch-s3-solution/lambdas/trigger-step-function-workflow/example_usage.py
+++ lambda-batch-s3-solution/lambdas/trigger-step-function-workflow/example_usage.py
@@ -2,7 +2,7 @@ import os
 import requests
 
 api_gateway_endpoint = os.environ.get("API_GATEWAY_ENDPOINT")
-endpoint = f"{api_gateway_endpoint}/run-inference?user_id=user1&object_name=lambda_function.py"
+endpoint = f"{api_gateway_endpoint}/run-inference?user_id=123&object_name=samplevideo.mp4"
 
 resp = requests.get(endpoint)
 print(resp)
