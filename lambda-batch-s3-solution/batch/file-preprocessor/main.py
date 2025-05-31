83c288b8b0fdb88e3f903fe185645c6c55d7d3b4
ahsin-s <ashabbi00@gmail.com>
2025-05-30 23:00:59 -0400

    update file-preprocessor to use shared code module

--- lambda-batch-s3-solution/batch/file-preprocessor/main.py
+++ lambda-batch-s3-solution/batch/file-preprocessor/main.py
@@ -5,40 +5,9 @@ import datetime
 
 import boto3
 import ffmpeg
-from botocore.client import ClientError
 
-s3_client = boto3.client("s3")
-
-
-def download_s3(bucket: str, key: str) -> str:
-    logging.info(f"Downloading from {bucket} {key}")
-    temp_file_path = os.path.basename(key)
-    s3_client.download_file(bucket, key, temp_file_path)
-    logging.info(f"Successfully downloaded the object to {temp_file_path}")
-    return temp_file_path
-
-
-def upload_s3(file_name, bucket, object_name=None):
-    """Upload a file to an S3 bucket
-
-    :param file_name: File to upload
-    :param bucket: Bucket to upload to
-    :param object_name: S3 object name. If not specified then file_name is used
-    :return: True if file was uploaded, else False
-    """
-
-    # If S3 object_name was not specified, use file_name
-    if object_name is None:
-        object_name = os.path.basename(file_name)
-
-    try:
-        logging.info(f"Uploading file to {object_name}")
-        response = s3_client.upload_file(file_name, bucket, object_name)
-    except ClientError as e:
-        logging.error(e)
-        return False
-    logging.info("Object successfully uploaded")
-    return True
+from shared_constructs.config import LambdaBatchS3SolutionConfig
+from shared_constructs.aws.s3 import download_s3, upload_s3
 
 
 def determine_if_conversion_needed(filename):
@@ -51,6 +20,7 @@ def determine_if_conversion_needed(filename):
 
 def extract_audio(video_path, audio_path, codec='mp3'):
     try:
+        print("video path: ", video_path)
         ffmpeg.input(video_path).output(audio_path, acodec=codec).run()
         print(f"Audio extracted and saved to {audio_path}")
     except ffmpeg.Error as e:
@@ -84,14 +54,16 @@ def main():
     key = sys.argv[2]
     job_id = sys.argv[3]
     user_id = sys.argv[4]
-    file = download_s3(bucket, key)
+
+    raw_upload_path = LambdaBatchS3SolutionConfig.get_raw_file_upload_path(user_id, key)
+    file = download_s3(bucket, raw_upload_path)
 
     converted_filename = os.path.basename(file)
     if determine_if_conversion_needed(file):
         converted_filename = converted_filename.split(".")[0] + ".mp3"
         extract_audio(file, converted_filename)
 
-    upload_key = f"converted/{user_id}/{job_id}/{converted_filename}"
+    upload_key = LambdaBatchS3SolutionConfig.get_preprocessed_path(user_id, job_id, converted_filename)
 
     if upload_s3(converted_filename, bucket, upload_key):
         if update_conversion_metadata_table(bucket, upload_key, job_id, user_id):
